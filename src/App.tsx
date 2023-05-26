3225a922532cdece93f482759281f3c8b3cbd6b1
japhheth <japhheth@gmail.com>
2023-05-26 15:46:24 +0100

    setup react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { ChakraProvider, theme, Text } from "@chakra-ui/react";
+import AppRouter from "./components/AppRouter";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Text>Hello</Text>
+    <AppRouter />
   </ChakraProvider>
 );
--- src/components/AppRouter/index.tsx
+++ src/components/AppRouter/index.tsx
@@ -1,7 +1,17 @@
-import { FC } from "react";
+import { FC, lazy } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// Countries
+const Countries = lazy(() => import("../../pages/Countries"));
 
 const AppRouter: FC = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Countries />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default AppRouter;
