2869c9e8c2177a97dcb6803a12ca7ccd0699c885
japhheth <japhheth@gmail.com>
2023-05-28 01:54:56 +0100

    Setup redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Provider } from "react-redux";
 import { ChakraProvider, theme } from "@chakra-ui/react";
+import { store } from "./store";
 import AppRouter from "./router";
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <AppRouter />
-  </ChakraProvider>
+  <Provider store={store}>
+    <ChakraProvider theme={theme}>
+      <AppRouter />
+    </ChakraProvider>
+  </Provider>
 );
